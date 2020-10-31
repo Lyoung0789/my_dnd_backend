73780f451f16cc899e203034b0998e52c5f9114f
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-31 17:35:23 -0400

    Adding schema for Character sheet migration. Creating registrations_controller so we can sign up a user

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ApplicationRecord
+    belongs_to :user
+    validates_presence_of :name, :class, :race
 end
--- db/migrate/20201031022902_create_characters.rb
+++ db/migrate/20201031022902_create_characters.rb
@@ -1,7 +1,41 @@
 class CreateCharacters < ActiveRecord::Migration[6.0]
   def change
     create_table :characters do |t|
+      t.string :name
+      t.string :class
+      t.integer :level
+      t.string :background
+      t.string :faction
+      t.string :race
+      t.string :alignment
+      t.integer :xp
+      t.integer :inspiration
+      t.integer :proficiency_bonus
+      t.integer :armor_class 
+      t.integer :initiative
+      t.integer :speed
+      t.integer :hp_max
+      t.integer :hp_current
+      t.integer :hp_temporary
+      t.integer :hit_dice_total
+      t.integer :hit_dice
+      t.string :personality_traits
+      t.string :ideals
+      t.string :bonds
+      t.string :flaws
+      t.string :features_traits
+      t.string :proficincy_and_languages
 
+      t.integer :age 
+      t.string :height 
+      t.integer :weight 
+      t.string :eyes
+      t.string :skin 
+      t.string :hair
+      t.string :allies_and_organization
+      t.string :character_backstory
+
+      t.integer :user
       t.timestamps
     end
   end
