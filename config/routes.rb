d042c558b6db38b64f444cd0d19d4a9188ea0598
Lyoung0789 <Lestermyoung07@gmail.com>
2020-11-02 20:33:32 -0500

    creating sessions controller and a concern to handle current_user. Also creating routes for the sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "static#home"
   resource :registrations, only: [:create]
+  resource :sessions, only: [:create]
+  delete :logout, to "sessions#logout"
+  get :loggedin, to "sessions#logged_in"
+  
 end
