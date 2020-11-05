93e2e8d7a5104b614e2e8036ee600d08d12ab245
Lyoung0789 <Lestermyoung07@gmail.com>
2020-11-04 22:46:54 -0500

    adding a player_name params into the registrations_controller. There already is migration column for it.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < ApplicationController
     def create 
-        user = User.create!(email: params["user"]["email"], password: params["user"]["password"], password_confirmation: params["user"]["password_confirmation"])
+        user = User.create!(email: params["user"]["email"], player_name: params["user"]["player_name"], password: params["user"]["password"], password_confirmation: params["user"]["password_confirmation"])
         if user
             session[:user_id] = user.id
             render json: {
